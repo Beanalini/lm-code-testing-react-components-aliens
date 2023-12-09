be225648f980bde9df33f5f402b7f06480ed1f4f
Beanalini <dat826@gmail.com>
2023-12-09 15:57:27 +0000

    feat: add SpeciesName component

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react';
-import W12MHeader from './W12MHeader';
+import { useState } from "react";
+import W12MHeader from "./W12MHeader";
+import SpeciesName from "./species-name";
 
 const W12MForm = () => {
-	return (
-		<section className='w12MForm'>
-			<W12MHeader />
-			{/* REST OF FORM GOES HERE */}
-		</section>
-	);
+  const [speciesName, setSpeciesName] = useState<string>("plants");
+  return (
+    <section className="w12MForm">
+      <W12MHeader />
+      {/* REST OF FORM GOES HERE */}
+      <SpeciesName
+        speciesName={speciesName}
+        onChangeSpeciesName={(value) => setSpeciesName(value)}
+      />
+    </section>
+  );
 };
 
 export default W12MForm;
