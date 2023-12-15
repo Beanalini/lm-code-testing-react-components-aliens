75286fdecdc3e340df73792dbf92fa21e1a57082
Beanalini <dat826@gmail.com>
2023-12-15 10:16:05 +0000

    add validation tests for validateSpeciesName function

---
+++ src/components/validate/validate-species-name.test.ts
@@ -0,0 +1,40 @@
+import { validateSpeciesName, error1, error2 } from "./validateSpeciesName";
+
+describe("Test validateSpeciesName function", () => {
+  test("Given the required props When the input is the valid number of characters Then the error message array should be empty", () => {
+    expect(validateSpeciesName("Daleks")).toEqual([]);
+    expect(validateSpeciesName("The TimeLords")).toEqual([]);
+    expect(validateSpeciesName("Ood")).toEqual([]);
+    expect(validateSpeciesName("Sister of Plenitudeeeee")).toEqual([]);
+  });
+
+  test("Given the required props When the input is the invalid number of characters Then the error message array should contain error2", () => {
+    expect(validateSpeciesName("Io")).toEqual([error2]);
+    expect(validateSpeciesName("The TimeLords The TimeLords")).toEqual([
+      error2,
+    ]);
+    expect(validateSpeciesName("I")).toEqual([error2]);
+  });
+
+  test("Given the required props When the input includes special characters or numbers Then  the error message array should contain error1", () => {
+    expect(validateSpeciesName("Daleks2000")).toEqual([error1]);
+    expect(validateSpeciesName("The % TimeLords!")).toEqual([error1]);
+    expect(validateSpeciesName("Ood 650!")).toEqual([error1]);
+    expect(validateSpeciesName("Sister of Plenitude!?!")).toEqual([error1]);
+  });
+  test("Given the required props When the input includes special characters or numbers and an invalid character length Then  the error message array should contain error1 and error2", () => {
+    expect(validateSpeciesName("Daleks2000??? ???&&££$7878")).toEqual([
+      error1,
+      error2,
+    ]);
+    expect(validateSpeciesName("The % TimeLords! 20202020")).toEqual([
+      error1,
+      error2,
+    ]);
+    expect(validateSpeciesName("*5")).toEqual([error1, error2]);
+    expect(validateSpeciesName("£££56Sister of Plenitude!?!")).toEqual([
+      error1,
+      error2,
+    ]);
+  });
+});
--- src/components/validate/validateSpeciesName.ts
+++ src/components/validate/validateSpeciesName.ts
@@ -1,12 +1,12 @@
+export const error1 = "No numbers or special characters allowed!";
+export const error2 = "Name must be between 3 and 23 characters only!";
 export const validateSpeciesName: (speciesName: string) => string[] = (
   speciesName
 ) => {
   //declare errorMessage array using the array constructor
   let errMessage = Array<string>();
 
-  const pattern = /[^A-Za-z ]/gi;
-  const error1 = "No numbers or special characters allowed!";
-  const error2 = "Name must be between 3 and 23 characters only!";
+  const pattern = /[^A-Za-z ]/g;
 
   if (pattern.test(speciesName)) {
     errMessage = [...errMessage, error1];
